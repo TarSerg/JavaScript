d12e026567577e2308394140ac1506f05a142e69
asd <beltar@ukr.net>
2023-05-01 22:55:09 +0300

    JS lesson1

---
+++ lesson1/mainlesson1.js
@@ -0,0 +1,55 @@
+
+
+let one = 'hello';
+let two = 'owu';
+let three = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+let k =  16;
+let l = true;
+let m = false;
+
+
+console.log(one)
+console.log(two)
+console.log(three)
+console.log(d)
+console.log(e)
+console.log(f)
+console.log(g)
+console.log(h)
+console.log(i)
+console.log(j)
+console.log(k)
+console.log(l)
+console.log(m)
+
+let firstName = 'Tarasevich';
+let middleName = 'Sergey';
+let lastName = 'Petrovich';
+
+let person = `${firstName} ${middleName} ${lastName}`;
+console.log(person)
+
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof (a))
+console.log(typeof (b))
+console.log(typeof (c))
+
+let name= prompt( 'Enter your name:' );
+console.log(name);
+
+let fatherland= prompt( 'Enter your fatherland:' );
+console.log(fatherland);
+
+let age= prompt( 'Enter your age:' );
+console.log(age);
+
